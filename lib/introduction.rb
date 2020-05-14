ef04642fc3f4ca90ee0c0cbdf789847beeeef3c6
Matine temory <temorymatine@gmail.com>
2020-05-14 20:33:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,12 @@ def introduction(name="john")
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language(name="bill",language="french")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name="bob", language="Ruby")
+  
 introduction
 
 
